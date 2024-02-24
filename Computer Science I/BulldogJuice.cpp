33a1f737b116e0ad27735221bfee58be7fcb8477
Eddy Koundjou <ekoundjou@gmail.com>
2024-02-24 16:52:54 -0500

    Creating a cash register

---
+++ Computer Science I/BulldogJuice.cpp
@@ -0,0 +1,21 @@
+//This project is a juice store that sells juice to customers
+// Name: eddy Koundjou Yem
+// 
+#include <iostream>
+//functions we need
+void collectOrder(); 
+
+void menu(); 
+
+int main ()
+{
+    //menu items
+}
+
+void menu()
+{
+    int orangeJuice, appleJuice, orppleJuice, water;
+    double waterPrice = 1.00; 
+    double orangeJuicePrice, appleJuicePrice = 2.00; 
+
+}
