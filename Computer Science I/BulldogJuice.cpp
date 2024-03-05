23f9b894d1585d06cf060f4937dda34fa8c3d5c8
Eddy Koundjou <ekoundjou@gmail.com>
2024-03-05 12:40:24 -0500

    Added OrderProcessing and collectOrder function

--- Computer Science I/BulldogJuice.cpp
+++ Computer Science I/BulldogJuice.cpp
@@ -2,20 +2,38 @@
 // Name: eddy Koundjou Yem
 // 
 #include <iostream>
+using namespace std; 
 //functions we need
 void collectOrder(); 
 
 void menu(); 
 
-int main ()
-{
+int main (){
     //menu items
+    return 0; 
 }
 
-void menu()
-{
+void menu(){
     int orangeJuice, appleJuice, orppleJuice, water;
     double waterPrice = 1.00; 
     double orangeJuicePrice, appleJuicePrice = 2.00; 
 
+}
+
+double orderProcessing(int orangeJuice, int appleJuice, int orppleJuice, int water, double waterPrice, double appleJuicePrice, double orangeJuicePrice){
+    double tax = 0.06; 
+    double totalCost = 0.00;
+
+    double totalOrangeCost = orangeJuice *orangeJuicePrice;
+    double totalAppleCost = appleJuice * appleJuicePrice; 
+    double totalWaterCost = water * waterPrice; 
+
+    double cost = (totalOrangeCost+totalAppleCost+totalWaterCost);
+    totalCost = cost + (0.06 * cost); 
+
+    return totalCost; 
+} 
+
+void collectOrder() {
+    cout << "What would you like to order today?" << endl;
 }
