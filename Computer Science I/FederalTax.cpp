7c709aefa0e0588cd515504e1e900f2d9d5a1a04
Eddy Koundjou <ekoundjou@gmail.com>
2024-04-15 10:20:49 -0400

    Creating a program that can calculate federal taxes owed

---
+++ Computer Science I/FederalTax.cpp
@@ -0,0 +1,13 @@
+//A program that can calculate the federal tax for a single person based on their income.
+#include <iostream>
+using namespace std;
+
+class FederalTax {
+    void getData();
+    void taxAmount();
+};
+
+int main() {
+
+  return 0;   
+}
