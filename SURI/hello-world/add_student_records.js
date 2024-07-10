75e1475ac0838b467c5b0508ceb4d831112488b3
Eddy Koundjou <ekoundjou@gmail.com>
2024-07-10 15:40:55 -0400

    Creating student records

---
+++ SURI/hello-world/add_student_records.js
@@ -0,0 +1,49 @@
+const {Sequelize, DataTypes} = require("sequelize");
+
+const sequelize = new Sequelize(
+   'sample_student_db',
+   'root',
+   'Jumpman17!',
+    {
+      host: 'localhost',
+      dialect: 'mysql'
+    }
+   );
+
+sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+}).catch((error) => {
+    console.error('Unable to connect to the database: ', error);
+});
+
+const Student = sequelize.define("students", {
+    student_id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
+
+const student_data = [
+    {name : "John Baker"},
+    {name : "Max Butler"},
+    {name : "Ryan Fisher"},
+    {name : "Robert Gray"},
+    {name : "Sam Lewis"}
+]
+
+sequelize.sync({ force: true }).then(() => {
+
+    Student.bulkCreate(student_data, { validate: true }).then((result) => {
+        console.log(result);
+    }).catch((error) => {
+        console.log(error);
+    });
+
+}).catch((error) => {
+    console.error('Unable to create table : ', error);
+});
---
+++ SURI/hello-world/array_raw_query.js
@@ -0,0 +1,28 @@
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(
+ 'sample_student_db',
+ 'root',
+ 'Jumpman17!',
+  {
+    host: 'localhost',
+    dialect: 'mysql'
+  }
+);
+
+sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+ }).catch((error) => {
+    console.error('Unable to connect to the database: ', error);
+ });
+
+sequelize.query(
+    'SELECT * FROM students WHERE student_id = ?',
+    {
+      replacements: ['d0fcf846-df00-40aa-b236-5dca1621b35b'],
+      type: sequelize.QueryTypes.SELECT
+    }
+).then(result => {
+    console.log(result);
+}).catch((error) => {
+    console.error('Failed to insert data : ', error);
+});
