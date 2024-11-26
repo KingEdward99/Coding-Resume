f362b3d24bf2c9d8d8623fdaafe8c5f648cde1ce
Eddy Koundjou <ekoundjou@gmail.com>
2024-11-25 19:41:34 -0500

    Hosting a html page on my server

--- Database Programming /first-servers/htmlFile.js
+++ Database Programming /first-servers/htmlFile.js
@@ -1,11 +1,39 @@
 const http = require("http");
+const fs = require('fs').promises;
 
 const host = 'localhost';
 const port = 8000;
 
-const requestListener = function (req, res) {};
+let indexFile;
+const requestListener = function (req, res) {
+    res.setHeader("Content-Type", "text/html");
+    res.writeHead(200);
+    res.end(indexFile);
+    fs.readFile(__dirname + "/index.html")
+    .then(contents => {
+        res.setHeader("Content-Type", "text/html");
+        res.writeHead(200);
+        res.end(contents);
+    })
+    .catch(err => {
+        res.writeHead(500);
+        res.end(err);
+        return;
+    });
+};
 
 const server = http.createServer(requestListener);
+fs.readFile(__dirname + "/index.html")
+    .then(contents => {
+        indexFile = contents;
+        server.listen(port, host, () => {
+            console.log(`Server is running on http://${host}:${port}`);
+        });
+    })
+    .catch(err => {
+        console.error(`Could not read index.html file: ${err}`);
+        process.exit(1);
+    });
 server.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
 });
