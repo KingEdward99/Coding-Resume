cda0e1905026ef863529737d145f253efeda1f68
Eddy Koundjou <ekoundjou@gmail.com>
2024-12-02 16:48:50 -0500

    Adding more books to our server

--- Database Programming /first-servers/routes.js
+++ Database Programming /first-servers/routes.js
@@ -4,13 +4,21 @@ const host = 'localhost';
 const port = 8000;
 
 const books = JSON.stringify([
-    { title: "The Alchemist", author: "Paulo Coelho", year: 1988 },
-    { title: "The Prophet", author: "Kahlil Gibran", year: 1923 }
+    {title: "Boulder", author: " Eva Baltasar", year: 2020 },
+    {title: "Whale", author: "Cheon Myeong-kwan", year: 1923 },
+    {title: "The Gospel According to the New World", author: "Maryse Conde", year: 2021},
+    {title: "Standing Heavy",author: "Gauz" , year: 2014},
+    {title: "Time Shelter", author: "Georgi Gospondinov", year: 2020},
+    {title: "Is Mother Dead", author: "Vigdis Hjorth", year: 2020}
 ]);
 
 const authors = JSON.stringify([
-    { name: "Paulo Coelho", countryOfBirth: "Brazil", yearOfBirth: 1947 },
-    { name: "Kahlil Gibran", countryOfBirth: "Lebanon", yearOfBirth: 1883 }
+    { name: "Eva Baltasar", countryOfBirth: "Spain", yearOfBirth: 1978 },
+    { name: "Kahlil Gibran", countryOfBirth: "South Korea", yearOfBirth: 1964},
+    { name: "Maryse Conde", countryOfBirth: "Guadeloupe", yearOfBirth: 1934},
+    { name: "Gauz", countryOfBirth: "Ivory Coast", yearOfBirth: 1971},
+    { name: "Georgi Gospondinov", countryOfBirth: "Bulgaria", yearOfBirth: 1968},
+    { name:  "Vigdis Hjorth", countryOfBirth: "Norway", yearOfBirth: 1959}
 ]);
 
 const requestListener = function (req, res) {
