bdb31dcf0074d108e7d4578a8a13f34059a01dea
Eddy Koundjou <ekoundjou@gmail.com>
2025-08-04 18:09:45 -0400

    Asking for customer payment

--- Computer Science I/BulldogJuice.cpp
+++ Computer Science I/BulldogJuice.cpp
@@ -1,6 +1,7 @@
 //This project is a juice store that sells juice to customers
 // Name: Eddy Koundjou Yem
 #include <iostream>
+#include <iomanip> 
 using namespace std; 
 
 //Creating a class called BulldogJuice
@@ -8,12 +9,15 @@ class BulldogJuice {
  public:
     //items and prices
     double waterPrice = 1.00;
-    double orppleJuicePrice, orangeJuicePrice, appleJuicePrice = 2.00;
+    double orppleJuicePrice = 2.00;
+    double orangeJuicePrice = 2.00;
+    double appleJuicePrice = 2.00;
 
     //functions
-    void collectOrder(int& orangeJuice, int& appleJuice, int& orppleJuice, int& water);
     void menu(); 
-    double orderProcessing(int& orangeJuice, int& appleJuice, int& orppleJuice, int& water, double& waterPrice, double& appleJuicePrice, double& orangeJuicePrice);
+    void collectOrder(int& orangeJuice, int& appleJuice, int& orppleJuice, int& water);
+    double orderProcessing(int orangeJuice, int appleJuice, int orppleJuice, int water, double waterPrice, double appleJuicePrice, double orangeJuicePrice);
+    void customerPaying(double& totalCost);
 };
 
 int main() {
@@ -21,53 +25,71 @@ int main() {
     BulldogJuice JuiceStore; 
 
     //menu items and prices 
-    int orangeJuice, appleJuice, orppleJuice, water; 
+    int orangeJuice = 0;
+    int appleJuice = 0;
+    int orppleJuice = 0;
+    int water = 0; 
     double waterPrice = 1.00;
-    double orppleJuicePrice, orangeJuicePrice, appleJuicePrice = 2.00;
-
+    double appleJuicePrice = 2.00;
+    double orangeJuicePrice = 2.00;
+    
     //calling the functions
     JuiceStore.menu(); 
     JuiceStore.collectOrder(orangeJuice, appleJuice, orppleJuice, water);
-    JuiceStore.orderProcessing(orangeJuice, appleJuice, orppleJuice, water, waterPrice, appleJuicePrice, orangeJuicePrice);
+
+    double totalCost = JuiceStore.orderProcessing(orangeJuice, appleJuice, orppleJuice, water, waterPrice, appleJuicePrice, orangeJuicePrice);
+    JuiceStore.customerPaying(totalCost); 
     return 0; 
 }
 
 //Presenting the menu to the customer
 void BulldogJuice::menu(){
     cout << "Welcome to the Bulldog Juice Store." << endl;
-    cout << "The following is our menu." << endl; 
-    cout << " Orange Juice: $2.00" << endl;
-    cout << " Apple Juice: $2.00" << endl;
-    cout << " Orpple Juice: $2.00" << endl;
-    cout << " Water: $1.00" << endl; 
+    cout << "Here's our menu:" << endl; 
+    cout << " Orange Juice: $" << fixed << setprecision(2) << orangeJuicePrice << endl;
+    cout << " Apple Juice: $" <<  appleJuicePrice << endl;
+    cout << " Orpple Juice: $" << orppleJuicePrice << endl;
+    cout << " Water: $" << waterPrice << endl << endl; 
 }
 
 //Collecting the order from the customer
 void BulldogJuice::collectOrder(int& orangeJuice, int& appleJuice, int& orppleJuice, int& water) {
     //collecting the order from the customer
-    cout << "What would you like to order today?" << endl;
-    cout << "How much apple juice do you want? " << endl; 
+    cout << "Please enter the quantity for each item: " << endl;
+    cout << "Apple Juice:  " << endl; 
     cin >> appleJuice;
-    cout << "How much orange juice do you want?" << endl; 
+    cout << "Orange Juice: " << endl; 
     cin >> orangeJuice; 
-    cout << "How much orpple juice do you want?" << endl;
+    cout << "Orpple Juice: " << endl;
     cin >> orppleJuice; 
-    cout << "How much water do you want?" << endl;
+    cout << "Water: " << endl;
     cin >> water; 
 }
 
 //Processing the customer's order
-double BulldogJuice::orderProcessing(int& orangeJuice, int& appleJuice, int& orppleJuice, int& water, double& waterPrice, double& appleJuicePrice, double& orangeJuicePrice){
+double BulldogJuice::orderProcessing(int orangeJuice, int appleJuice, int orppleJuice, int water, double waterPrice, 
+double appleJuicePrice, double orangeJuicePrice){
     double tax = 0.06; 
-    double totalCost = 0.00;
 
     double totalOrangeCost = orangeJuice *orangeJuicePrice;
     double totalAppleCost = appleJuice * appleJuicePrice; 
     double totalWaterCost = water * waterPrice; 
 
     long double cost = (totalOrangeCost+totalAppleCost+totalWaterCost);
-    totalCost = cost + (0.06 * cost); 
+    double totalCost = cost + (0.06 * cost); 
     cout << "The total cost of your order is: $" << totalCost << endl;
 
     return totalCost; 
 } 
+
+void BulldogJuice::customerPaying(double& totalCost){
+    double payment = 0.0;
+    
+    cout << "How much will you be paying?" << endl;
+    cin >> payment;
+
+    double change = payment - totalCost; 
+
+    cout << "You paid $" << payment << endl;
+    cout << "Your change is $" << change << endl;
+}
