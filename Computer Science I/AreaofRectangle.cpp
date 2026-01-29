98a02059183ba54ef825d85cdd459e42d3f29a9c
Eddy Koundjou <ekoundjou@gmail.com>
2026-01-28 23:22:37 -0500

    updating CS 1 and CS 2 programs

---
+++ Computer Science I/AreaofRectangle.cpp
@@ -0,0 +1,19 @@
+/*
+Name: Eddy Koundjou Yem 
+Program Description: This program calculates the permieter and the area of a rectangle
+Date: April 15, 2024
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+
+{   //necessary variables
+    int length, width = 4; 
+    
+    //perimeter of a rectangle 
+    int perimeter = 2 * (length + width);
+
+    int area = length * width; 
+}
