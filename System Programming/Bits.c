63f1c36a0e065d714ae58a5a177a263d70e0b0e8
Eddy Koundjou <ekoundjou@gmail.com>
2026-02-10 23:47:47 -0500

    Adding comments to C program

--- System Programming/Bits.c
+++ System Programming/Bits.c
@@ -1,17 +1,35 @@
 /* Name Eddy Koundjou Yem
   Date: September 15 
+
+  Description:
+  This program allows the user to manipulate bits of a 32-bit integer.
+  The user can:
+    1. Set (turn on) a specific bit
+    2. Clear (turn off) a specific bit
 */
 #include <stdio.h>
 
 int main() {
+
+  //Stores the number whose bits will be modified
   int num = 0;
+
+  //Stores the bit position the user wants to modify
   int bit_num = 0;
+
+  //Stores the user's choice 
   int choice;
 
+  //Infinite loop to continuously allow to bit manipulation
   while(1)
     {
+      //Print the 32-bit binary representation of num 
+      //Loop starts from the most significant bit (31)
+      //and goes down to the least significant bit (0)
       for (int i = 31; i >= 0; i--)
         {
+          //Check if bit i is set using bitwise AND 
+          //
           printf("%i",((num & (1 << i))?1:0));
         }
       printf("\n Enter 1. to set bit:");
@@ -34,8 +52,6 @@ int main() {
       if (choice == 2)
       {
         num &= ~(1 << bit_num);
-      }
-  
-        
+      }  
     }
 }
