fae86c908827ca3cff39302eb1e61a9c9b292cd4
Eddy Koundjou <ekoundjou@gmail.com>
2026-02-13 23:15:06 -0500

    adjusting some folders

--- Discrete Math/Set Comparison.py
+++
@@ -1,9 +0,0 @@
-#This program compares two sets 
-#1 Let's build two empty sets
-#2 Lets build operations 
-#3 Include user input
-
-#Set building 
-Set1 = {1,2,3,4}
-Set2 = {5,6,7,8}
-print(Set1, Set2)
