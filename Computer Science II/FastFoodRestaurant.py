ccd6a4fc4c1618ac69dc53c1d01c637ea176eb2d
Eddy Koundjou <ekoundjou@gmail.com>
2026-02-19 02:03:55 -0500

    Creating Fast Food python file

---
+++ Computer Science II/FastFoodRestaurant.py
@@ -0,0 +1,14 @@
+# This program enables users to buy wings from The Pub at Bowie State
+
+#Presenting the menu to the customer
+def menu():
+    print("Welcome to The Pub")
+    print(" We have bbq wings for $4")
+    print ("Our wings combo is $10")
+    print("Our cornbread is $2")
+
+#The options
+def foodprince():
+    four_wings = 4.00
+    wings_combo = 10.00
+    cornbread = 2.00
