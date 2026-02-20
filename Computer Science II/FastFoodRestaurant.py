abd5d4ab222c583e48de915a0b5b960bc5486733
Eddy Koundjou <ekoundjou@gmail.com>
2026-02-19 23:15:06 -0500

    Creating the userOrder function and orderProcessing function

--- Computer Science II/FastFoodRestaurant.py
+++ Computer Science II/FastFoodRestaurant.py
@@ -3,12 +3,37 @@
 #Presenting the menu to the customer
 def menu():
     print("Welcome to The Pub")
-    print(" We have bbq wings for $4")
-    print ("Our wings combo is $10")
+    print("We have chicken wings for $1 per wings")
+    print("The wing flavors are: barbecue, zing, and lemon pepper")
     print("Our cornbread is $2")
+    print("Our drink options are Sprite, Coca-Cola, and water")
 
 #The options
-def foodprince():
-    four_wings = 4.00
-    wings_combo = 10.00
+def foodprice():
+    wings = 1.00
     cornbread = 2.00
+    drink = 1.00
+
+#Collecting the user's order
+def userOrder():
+    userOrder = input("What do you want to order?")
+
+    wings_quantity = int(input("How many wings do you want?"))
+    wings_flavor = input("What flavor do you want?")
+
+    cornbread = int(input("How much cornbread do you want"))
+
+    drink_flavor = input("Which drink do you want?")
+    drink_quantity = input(int("How many drinks do you want?"))
+
+def orderProcessing(wings_quantity, cornbread, drink_quantity):
+    wings_total_price = 1.00 * wings_quantity
+    cornbread_total_price = 2.00 * cornbread
+    drink_total_price = 1.00 * drink_quantity
+
+#Presenting the menu
+menu()
+
+#Collecting the user order
+userOrder()
+
