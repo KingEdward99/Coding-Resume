44667cb4912f7a7b6f89c0be1157437e9be476f5
Eddy Koundjou <ekoundjou@gmail.com>
2026-03-14 17:18:21 -0400

    Created the combination and permutation function. Need to brush it up and make sure the equations are consistently correct

--- Discrete Math/Permutation_Combination_Calculator.py
+++ Discrete Math/Permutation_Combination_Calculator.py
@@ -1,33 +1,71 @@
 #Calculator that figures out the combination and permutation
 
 #C(n,r) = n!/(r!*(n-r)!)
+#P(n,r) = n!/(n-r)!
 
-#Combination function
-#Top Half
-n = int(input("Enter a number"))
-top_factorial = 1
+from math import perm
 
-for i in range(1, n+1):
-    top_factorial *= i
+def Perm_Comb_choice():
+    #initalizing the options
+    perm_or_comb = 0
+    perm_or_comb = int(input("Does the problem require a combination or a permutation?"))
 
-print("The factorial of ", n, " is ", top_factorial)
+    size = int(input("What is the quantity of the equation? (e.g 5 muffins, 10 students, 8 crayons)"))
+    order = int(input("How many places does the element have? (e.g 3 places, 5 flavors, etc"))
 
-#Bottom Left Half
-r = int(input("Enter a number"))
-BL_factorial = 1
+    return perm_or_comb, size, order
 
-for i in range(1,r+1):
-    BL_factorial *= i
+def combination():
+    #Combination function
+    #Top Half
+    n = int(input("Enter a number"))
+    top_factorial = 1
 
-print("The factorial of ", r, " is ", BL_factorial)
+    for i in range(1, n+1):
+        top_factorial *= i
 
-#Bottom Right Half
-BR_factorial = 1
-s = n-r
+    print("The factorial of ", n, " is ", top_factorial)
 
-for i in range(1,s+1):
-    BR_factorial *= i
+    #Bottom Left Half
+    r = int(input("Enter a number"))
+    BL_factorial = 1
 
-combination = top_factorial/(BL_factorial*BR_factorial)
+    for i in range(1,r+1):
+        BL_factorial *= i
 
-print("Combination is ",combination)
+    print("The factorial of ", r, " is ", BL_factorial)
+
+    #Bottom Right Half
+    BR_factorial = 1
+    s = n-r
+
+    for i in range(1,s+1):
+        BR_factorial *= i
+
+    combination = top_factorial/(BL_factorial*BR_factorial)
+    
+    print("Combination is ",combination)
+
+#Permutation Calculator
+#n!/(n-r)!
+def permutation():
+    #Permutation function
+    #Top Half
+    n = int(input("Enter a number"))
+    top_factorial = 1
+
+    for i in range(1, n+1):
+        top_factorial *= i
+
+    print("The factorial of ", n, " is ", top_factorial)
+
+    #Bottom Factorial
+    r = int(input("Enter a number"))
+    bot_factorial = n-r
+
+    for i in range(1,i+1):
+        bot_factorial *= i
+
+    permutation = top_factorial/(bot_factorial)
+    
+    print("Permutation is: ",permutation)    
