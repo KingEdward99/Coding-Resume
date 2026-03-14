48139f2e6f7186a94741224476f9ac7a5919cbf9
Eddy Koundjou <ekoundjou@gmail.com>
2026-03-14 17:18:51 -0400

    Getting ready to make a food ordering tablet system

---
+++ Software Engineering/Fast_Food_Restaurant/Fast_Food_Ordering.py
@@ -0,0 +1,8 @@
+#Ordering at a restaurant
+#Customer orders from their table and the food gets delivered to them
+#Using the same menu from my FastFoodRestaurant.py folder
+
+#Situation: we have tables that can order food and drinks
+#customers can order from 4 tables
+#they can order their food and drinks 
+#chef can receive the order...this is still under mental constructions
