675f83be331054bc2b47720f7ef1843efcabcd51
Eddy Koundjou <ekoundjou@gmail.com>
2026-03-15 01:21:10 -0400

    Preparing a new resident database using python dictionaries

---
+++ Computer Science I/Eddy's Apartment/Residents.py
@@ -0,0 +1,5 @@
+#This program reads a file that contains residents and their apartment numbers
+#It translates that information into a text file
+
+apartments = { "James Johnson": "A1",
+              "Jerome Crayton": "A2" }
