d9bae9cc45d94d1e384f5b9a415abd243ffe6dbb
Eddy Koundjou <ekoundjou@gmail.com>
2026-03-19 00:34:58 -0400

    Adding a deposit function

--- SURI/Bank.py
+++ SURI/Bank.py
@@ -3,3 +3,10 @@
 userMoney = 0
 
 print("Welcome to the bank!")
+
+def deposit_money():
+    deposit_amount = 0
+
+    deposit_amount = int(input("How much do you want to deposit"))
+
+    return deposit_amount
