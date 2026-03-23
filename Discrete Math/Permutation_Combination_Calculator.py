39c32ef78b46ec5dbff9fc3a831ba3e390461f41
Eddy Koundjou <ekoundjou@gmail.com>
2026-03-22 23:32:28 -0400

    Adding cyber fundamental future programs

--- Discrete Math/Permutation_Combination_Calculator.py
+++ Discrete Math/Permutation_Combination_Calculator.py
@@ -4,36 +4,24 @@
 #P(n,r) = n!/(n-r)!
 
 from math import perm
-
-def Perm_Comb_choice():
-    #initalizing the options
-    perm_or_comb = 0
-    perm_or_comb = int(input("Does the problem require a combination or a permutation?"))
-
-    size = int(input("What is the quantity of the equation? (e.g 5 muffins, 10 students, 8 crayons)"))
-    order = int(input("How many places does the element have? (e.g 3 places, 5 flavors, etc"))
-
-    return perm_or_comb, size, order
-
+#Combination calculator
+#C(n,r) = n!/(r!*(n-r)!)
 def combination():
     #Combination function
+    #Asking for the input 
+    n = int(input("Enter n (total items):"))
+    r = int(input("Enter r (items to arrange)"))
+    
     #Top Half
-    n = int(input("Enter a number"))
     top_factorial = 1
-
     for i in range(1, n+1):
         top_factorial *= i
 
-    print("The factorial of ", n, " is ", top_factorial)
 
     #Bottom Left Half
-    r = int(input("Enter a number"))
     BL_factorial = 1
-
     for i in range(1,r+1):
         BL_factorial *= i
-
-    print("The factorial of ", r, " is ", BL_factorial)
 
     #Bottom Right Half
     BR_factorial = 1
@@ -45,9 +33,10 @@ def combination():
     combination = top_factorial/(BL_factorial*BR_factorial)
     
     print("Combination is ",combination)
+    print(f"C({n}, {r}) = {combination}") 
 
 #Permutation Calculator
-#n!/(n-r)!
+#P(n,r) = n!/(n-r)!
 def permutation():
     #Permutation function: P(n,r) = n! / (n-r)!
     n = int(input("Enter n (total items): "))
@@ -79,7 +68,6 @@ def menu():
     return choice
 
 choice = menu()
-
 if choice == 1:
     combination()
 else:
