98168c5dbcf2134950b944183e30ae19cabb4949
Eddy Koundjou <ekoundjou@gmail.com>
2026-03-29 13:35:53 -0400

    Fixing the introduction; Adding necessary comments for each function; Added search function & menu function;

--- Computer Science I/Eddy's Apartment/Residents.py
+++ Computer Science I/Eddy's Apartment/Residents.py
@@ -1,14 +1,8 @@
-#This program reads a file that contains residents and their apartment numbers
-#It translates that information into a text file
+#This is an apartment management program. It allows the user to print current residents, add residents, remove residents, and search for residents
+#This program explores the usage of dictionaries
 
-"""
-four methods
-1. print current residents
-2. add residents
-3. remove residents
-4. search residents
-"""
-
+#Function that reads the file and enters it into the program 
+from os import remove
 
 def file_reading():
     apartments = {}
@@ -22,12 +16,14 @@ def file_reading():
     
     return apartments
 
+#Displays the current residents and their apartment number
 def print_residents():
     apartments = file_reading()
     print(apartments)
 
+#Adding residents to the apartment 
 def add_residents(apartments):
-    #The maximum residents are eight
+    #The maximum residents that can be added are eight
     if len(apartments) >= 8:
         print("There apartment list is too full. You cannot add more residents")
     else:
@@ -37,23 +33,57 @@ def add_residents(apartments):
         room = input("Enter the room number: ")
 
         apartments[name] = room
-    
+
+#Removing residents from their apartment    
 def remove_residents(apartments):
+    #Can't remove residnts from an empty apartment 
     if len(apartments) == 0:
-        print("The apartmets are empty. You cannot remove any more residents")
+        print("The apartments are empty. You cannot remove any more residents")
     else:
         print("Which resident do you want to remove from the apartment")
         name = input("Enter resident name: ")
 
+        #Checking to see if the user actually exists in the apartment
         if name in apartments: 
             apartments.pop(name)
             print(f"{name} has been removed.")
         else:
             print(f"{name} is not in the apartment")
 
-"""
-def search_residents():
+#Searching for a resident to see where they say 
+def search_residents(apartments):
+    name = input("Which resident are you looking for")
+    if name in apartments:
+        print(f"{name} is in room.")
+    else:
+        print(f"{name} is not in the apartment")
     print()
-"""
 
-print_residents()
+#Menu for the user
+def menu(apartments):
+    print("Welcome to the Eddy's Apartment database")
+    print("You can display, remove, add residents that are currently in the apartment")
+    print("To display the current residents, press 1")
+    print("To add a resident, press 2")
+    print("To remove a resident, press 3")
+    print("To search for a resident, press 4")
+    print("To exit the program, press -1")
+
+    choice = 0
+    while choice != -1:
+        choice = int(input("Which option do you want to pick"))
+        if choice == 1:
+            print_residents()
+        elif choice == 2:
+            add_residents(apartments)
+        elif choice == 3: 
+            remove_residents(apartments)
+        elif choice == 4: 
+            search_residents(apartments)
+        else:
+            print("Invalid option")
+        
+#Running the program 
+apartments = {}
+apartments = file_reading()
+menu(apartments)
