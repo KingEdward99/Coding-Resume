0234d94bc683305be3e04978e66e65a5e30830ae
Eddy Koundjou <ekoundjou@gmail.com>
2026-03-29 14:23:24 -0400

    Server is able to dispay error and read in files

--- SURI: Web3 Bank App/servers/App.js
+++ SURI: Web3 Bank App/servers/App.js
@@ -4,12 +4,22 @@
 
 //Using the http library from JS
 const http = require('http')
+const fs = require ('fs') // file handling import
 const port = 3000 //listening to port 3000
 
 //creating the server 
 const server = http.createServer(function(req,res) {
-    res.write("Hello Node")
-    res.end()
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    fs.readFile('index.html', function(error, data) {
+        if (error) {
+            res.writeHead(404)
+            res.write('Error: File Not Found')
+        }
+        else {
+            res.write(data)
+        }
+        res.end()
+    })
 })
 
 //Telling our server to listen to port and tell us if there's an error 
