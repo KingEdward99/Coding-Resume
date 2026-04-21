0f3fc2cbb2a588119e34a5feafb5c030e3ee7e36
Eddy Koundjou <ekoundjou@gmail.com>
2026-04-21 01:09:39 -0400

    Added user input and displayed the results

--- Computer Science I/AreaofRectangle.cpp
+++ Computer Science I/AreaofRectangle.cpp
@@ -1,6 +1,6 @@
 /*
 Name: Eddy Koundjou Yem 
-Program Description: This program calculates the permieter and the area of a rectangle
+Program Description: This program calculates the perimter and the area of a rectangle
 Date: April 15, 2024
 */
 
@@ -9,11 +9,21 @@ using namespace std;
 
 int main()
 
-{   //necessary variables
-    int length, width = 4; 
+{  
+    int length = 0;
+    int width = 0;
+
+    cout << "Enter the length" << endl;
+    cin >> length;
+    cout << "Enter the width" << endl;
+    cin >> width;
+
     
     //perimeter of a rectangle 
     int perimeter = 2 * (length + width);
 
     int area = length * width; 
+
+    cout << "The perimeter is: " << perimeter << endl; 
+    cout << "The area is: " << area << endl; 
 }
