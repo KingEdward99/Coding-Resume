19fbcd86a05c1e043d7eaed2a67e9c796bfa2f1b
Eddy Koundjou <ekoundjou@gmail.com>
2026-07-23 22:20:25 -0400

    can insert students in the linkedlist

--- Computer Science II/Waitlist/HousingWaitlist.cpp
+++ Computer Science II/Waitlist/HousingWaitlist.cpp
@@ -20,7 +20,7 @@ class Node {
         //Parameterized constructor
         Node (string name) {
             this->name = name;
-            this->next = nullptr;
+            next = nullptr;
         }
 };
 
@@ -34,4 +34,53 @@ class HousingWaitlist {
             head = nullptr;
         }
 
+        void insertStudentAtEnd(string name) {
+
+            //Creating the new student / node
+            Node* newStudent = new Node(name);
+
+            //Checking to see if the linked list is empty
+            if (head == nullptr) {
+                head = newStudent;
+                return;
+            }
+
+            //Store the head reference in a temporary variable
+            Node* last = head;
+            
+            //Traverse until you reach the last node
+            while(last->next != nullptr) {
+                last = last->next;
+            }
+
+            //Change the next pointer of the last node to the next new node
+            last->next = newStudent;
+        }
+
+        //Printing the linked list
+        void printWaitList() {
+            Node* current = head;
+            while(current != nullptr) {
+                cout << current->name;
+                if(current->next != nullptr) {
+                    cout << " -> ";
+                }
+
+                current = current->next;
+            }
+
+        cout << endl;
+    }
 };
+
+int main () {
+    //Creating the waitlist object
+    HousingWaitlist list;
+
+    list.insertStudentAtEnd("Dasean");
+    list.insertStudentAtEnd("Jeremy");
+
+    list.printWaitList();
+
+    return 0;
+}
